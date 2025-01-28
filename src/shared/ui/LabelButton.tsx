c6a59d79496b4d36443da37a8f9358a2a8099bdd
Vladyslav Sliusar <flickervladislav@gmail.com>
2025-01-28 22:25:47 +0200

    feat: add LabelButton component

---
+++ src/shared/ui/LabelButton.tsx
@@ -0,0 +1,108 @@
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import { PropsWithChildren } from 'react';
+
+import { Color } from '../theme/colors';
+
+type LabelButtonProps = PropsWithChildren<{
+  color: `${LabelButtonColor}`;
+}>;
+
+const enum LabelButtonColor {
+  WHITE = 'white',
+  BLUE = 'blue',
+  RED = 'red',
+  GREEN = 'green',
+}
+
+const LabelButtonColorToCSS = {
+  [LabelButtonColor.WHITE]: css`
+    --color-text: ${Color.WHITE_87};
+
+    &:hover,
+    &:focus {
+      --color-button: ${Color.WHITE_8};
+    }
+
+    &:active {
+      --color-button: ${Color.WHITE_16};
+    }
+
+    &:disabled {
+      --color-button: ${Color.WHITE_8};
+    }
+  `,
+  [LabelButtonColor.BLUE]: css`
+    --color-text: ${Color.BLUE_1};
+
+    &:hover,
+    &:focus {
+      --color-button: ${Color.BLUE_4};
+    }
+
+    &:active {
+      --color-button: ${Color.BLUE_3};
+    }
+
+    &:disabled {
+      --color-button: ${Color.BLUE_4};
+    }
+  `,
+  [LabelButtonColor.RED]: css`
+    --color-text: ${Color.RED_1};
+
+    &:hover,
+    &:focus {
+      --color-button: ${Color.RED_5};
+    }
+
+    &:active {
+      --color-button: ${Color.RED_4};
+    }
+
+    &:disabled {
+      --color-button: ${Color.RED_5};
+    }
+  `,
+  [LabelButtonColor.GREEN]: css`
+    --color-text: ${Color.GREEN_1};
+
+    &:hover,
+    &:focus {
+      --color-button: ${Color.GREEN_4};
+    }
+
+    &:active {
+      --color-button: ${Color.GREEN_3};
+    }
+
+    &:disabled {
+      --color-button: ${Color.GREEN_4};
+    }
+  `,
+};
+
+const StyledLabelButton = styled.button<LabelButtonProps>(({ color }) => {
+  return css`
+    padding: 4px 8px;
+    border-radius: 4px;
+
+    color: var(--color-text);
+    background-color: var(--color-button);
+
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.43;
+
+    cursor: pointer;
+
+    &:disabled {
+      opacity: 0.7;
+      cursor: not-allowed;
+    }
+
+    ${LabelButtonColorToCSS[color]}
+  `;
+});
+
+export { StyledLabelButton as LabelButton };
--- src/widgets/Buttons.tsx
+++ src/widgets/Buttons.tsx
@@ -4,6 +4,7 @@ import { JSX } from 'react';
 import { Button } from '~/shared/ui/Button';
 import { Icon } from '~/shared/ui/Icon';
 import { IconButton } from '~/shared/ui/IconButton';
+import { LabelButton } from '~/shared/ui/LabelButton';
 
 function Buttons(): JSX.Element {
   return (
@@ -26,6 +27,23 @@ function Buttons(): JSX.Element {
       <IconButton size="small" variant="filled" color="transparent">
         <Icon name="icon-plus" />
       </IconButton>
+
+      <LabelButton color="white">Neutral</LabelButton>
+      <LabelButton color="white" disabled>
+        Neutral
+      </LabelButton>
+      <LabelButton color="blue">Neutral</LabelButton>
+      <LabelButton color="blue" disabled>
+        Neutral
+      </LabelButton>
+      <LabelButton color="red">Neutral</LabelButton>
+      <LabelButton color="red" disabled>
+        Neutral
+      </LabelButton>
+      <LabelButton color="green">Neutral</LabelButton>
+      <LabelButton color="green" disabled>
+        Neutral
+      </LabelButton>
     </StyledWrapper>
   );
 }
