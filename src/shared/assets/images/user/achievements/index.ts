a97d57179e1a4bb17ea362f1dc7cc943c69d6174
Vladyslav Sliusar <flickervladislav@gmail.com>
2025-02-26 17:06:36 +0200

    feat: add user achievement images and avatar

---
+++ src/shared/assets/images/user/achievements/index.ts
@@ -0,0 +1,19 @@
+import accessCard from './access-card.png';
+import champagne from './champagne.png';
+import graduationCap from './graduation-cap.png';
+import law from './law.png';
+import mission from './mission.png';
+import petCare from './pet-care.png';
+import piggyBank from './piggy-bank.png';
+import values from './values.png';
+
+export const achievementImage = {
+  accessCard,
+  champagne,
+  graduationCap,
+  law,
+  mission,
+  petCare,
+  piggyBank,
+  values,
+};
---
+++ src/shared/assets/images/user/index.ts
@@ -0,0 +1,7 @@
+import { achievementImage } from './achievements';
+import avatarImage from './avatar.png';
+
+export const userImage = {
+  achievement: achievementImage,
+  avatar: avatarImage,
+};
