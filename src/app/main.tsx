cb064f8f5c9ba1c341709fec551d9611f75420b0
Vladyslav <flickervladislav@gmail.com>
2025-08-25 15:43:29 +0300

    feat: add analytics

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,7 +1,10 @@
+import { init } from '@plausible-analytics/tracker';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
 import { AppWithProviders } from './App';
+
+init({ domain: 'alterlands.com' });
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
